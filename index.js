f95e19bacfdde30d423f8de550f555564a1712d0
Garima <garima02304@gmail.com>
2024-03-13 19:01:16 +0530

    MelodyHub

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 
 
@@ -19,7 +19,7 @@ app.use('',(req,res)=>{
     res.render('index');
 });
 
-
-app.listen(8080,()=>{
-    console.log("Server is running at 3001");
+const port = 8080;
+app.listen(port,()=>{
+    console.log(`Server is running at ${port}`);
 });
